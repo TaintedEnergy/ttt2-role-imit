67805e76cca798651d7c4496e467b17354a00a98
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2022-02-01 16:26:36 -0600

    Added special syncing to allow Imitator to see allies

--- lua/terrortown/entities/roles/imitator/shared.lua
+++ lua/terrortown/entities/roles/imitator/shared.lua
@@ -193,6 +193,19 @@ if SERVER then
 			return false
 		end
 	end)
+
+	--Allow Imitator to see traitor allies
+	hook.Add("TTT2SpecialRoleSyncing", "TTT2ImitatorSeeTraitorTeam", function(ply, tbl)
+		if not ply.imit_has_voted then return end
+
+		for tra in pairs(tbl) do
+			if tra:IsTerror() and (tra:GetTeam() == TEAM_TRAITOR or tra:GetSubRoleData().defaultTeam == TEAM_TRAITOR or tra:GetSubRole() == ROLE_SPY) and tra ~= ply then
+				tbl[tra] = {ROLE_TRAITOR, TEAM_TRAITOR}
+			elseif ply == tra then
+				tbl[tra] = {tbl[tra][1], TEAM_TRAITOR}
+			end
+		end
+	end)
 end
 
 if CLIENT then
